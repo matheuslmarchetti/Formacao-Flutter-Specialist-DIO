ebda1fb1ba4caee437c97a052426b201bb45a251
matheuslmarchetti <lunguinhomarchetti@gmail.com>
2023-05-22 16:30:01 -0400

    Aprendendo fundamentos da liguaguem dart.

---
+++ primeiro_app_dart/bin/primeiro_app_dart.dart
@@ -0,0 +1,5 @@
+import 'package:primeiro_app_dart/primeiro_app_dart.dart' as primeiro_app_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${primeiro_app_dart.calculate()}!');
+}
---
+++ primeiro_app_dart/test/primeiro_app_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:primeiro_app_dart/primeiro_app_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
